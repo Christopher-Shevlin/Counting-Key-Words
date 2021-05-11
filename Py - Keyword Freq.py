6e82cf77326d1a92aec001a1fdcdf82b9d53d314
Christopher Shevlin <47715649+Christopher-Shevlin@users.noreply.github.com>
2021-05-11 17:37:12 +0100

    Commit

--- Py - Keyword Freq.py
+++ Py - Keyword Freq.py
@@ -1,10 +1,3 @@
-
-english_stops = set(stopwords.words('english'))
-# Retain alphabetic words: alpha_only
-alpha_only = [t for t in lower_tokens if t.isalpha()]
-# Remove all stop words: no_stops
-no_stops = [t for t in alpha_only if t not in english_stops]
-
 from collections import Counter
 line_text = """test test test test"""
 
